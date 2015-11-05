5496a3a934cfe28dbaac57c55434f008d12dc991
sandrockman <svc-student@RC335G03.student.directory.jccc>
2015-11-05 12:07:42 -0600

    unique player identities created.
    Will start on damage creating stuff next.
    
    https://www.youtube.com/watch?v=_u_0IphF33o&index=10&list=PLwyZdDTyvucyAeJ_rbu_fbiUtGOVY55BG

---
+++ Project-06/Assets/Scripts/Multiplayer/PlayerID.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerID : NetworkBehaviour {
+
+	[SyncVar]public string playerUniqueIdentity;
+	private NetworkInstanceId playerNetID;
+	private Transform myTransform;
+
+	public override void OnStartLocalPlayer()
+	{
+		GetNetIdentity ();
+		SetIdentity ();
+	}
+	// Use this for initialization
+	void Awake () {
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (myTransform.name == "" || myTransform.name == "Player(Clone)")
+			SetIdentity ();
+	}
+
+	[Client]
+	void GetNetIdentity()
+	{
+		playerNetID = GetComponent<NetworkIdentity> ().netId;
+		CmdGeneratePlayerIdentity (MakeUniqueIdentity ());
+	}
+
+	string MakeUniqueIdentity()
+	{
+		string uniquename = "Player " + playerNetID.ToString ();
+		return uniquename;
+	}
+
+	void SetIdentity()
+	{
+		if (!isLocalPlayer)
+			myTransform.name = playerUniqueIdentity;
+		else
+			myTransform.name = MakeUniqueIdentity();
+	}
+
+	[Command]
+	void CmdGeneratePlayerIdentity(string name)
+	{
+		playerUniqueIdentity = name;
+	}
+
+	//to begin shooting on lesson 8, go to
+	//https://www.youtube.com/watch?v=_u_0IphF33o&index=10&list=PLwyZdDTyvucyAeJ_rbu_fbiUtGOVY55BG
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
